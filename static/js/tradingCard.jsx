7a5cf0478fc41e61e15130d1cddc48e7266b5dd2
Stephanie Shih <stephk.shih@gmail.com>
2020-05-21 17:20:56 -0700

    Modified so that cards are being mounted without having to hard code each card individually

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,5 +1,56 @@
 "use strict";
 
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
+
 class TradingCard extends React.Component {
   render() {
     return (
@@ -12,57 +63,26 @@ class TradingCard extends React.Component {
   }
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
+class TradingCardContainer extends React.Component {
+  render() {
+    const tradingCards = [];
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
+    for (const currentCard of tradingCardData) {
+      tradingCards.push(
+        <TradingCard
+          name={currentCard.name}
+          skill={currentCard.skill}
+          imgUrl={currentCard.imgUrl}
+        />
+      );
+    }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
+    return (
+      <div>
+        {tradingCards}
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Merge"
-      skill="scavenging"
-      imgUrl="/static/img/merge.jpg"
-    />
-  ),
-  document.querySelector('#merge')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Seedpy"
-      skill="being the CEO of UberMelon"
-      imgUrl="/static/img/seedpy.jpg"
-    />
-  ),
-  document.querySelector('#seedpy')
-);
+ReactDOM.render(<TradingCardContainer />, document.querySelector('#container'));
